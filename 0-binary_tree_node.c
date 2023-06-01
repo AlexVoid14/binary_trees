f89cf13d65bbd01418132e73502778faa8d6b196
NyatiZw <appsmaid19@gmail.com>
2023-06-01 10:54:43 -0700

    betty check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,14 +12,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *root = malloc(sizeof(binary_tree_t));
-        if (!root)
-                return NULL;
+	binary_tree_t *root = malloc(sizeof(binary_tree_t));
+	if (!root)
+		return NULL;
 
-        root->parent = parent;
-        root->left = NULL;
-        root->right = NULL;
-        root->n = value;
+	root->parent = parent;
+	root->left = NULL;
+	root->right = NULL;
+	root->n = value;
 
-        return (root);
+	return (root);
 }
