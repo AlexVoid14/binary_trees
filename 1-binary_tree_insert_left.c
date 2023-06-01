b635e7beb7c5ffffd6b03ce662717b7314f98c8f
NyatiZw <appsmaid19@gmail.com>
2023-06-01 16:56:25 -0700

    binary tree corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root = malloc(sizeof(binary_tree_t));
+
 	if (root == NULL || parent == NULL)
 		return (NULL);
 
