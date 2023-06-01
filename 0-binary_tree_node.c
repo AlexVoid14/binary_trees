a599c7d2e72739414a9af122bbc5244fc82f44ce
NyatiZw <appsmaid19@gmail.com>
2023-06-01 16:54:18 -0700

    binary tree corrections

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,8 +13,9 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root = malloc(sizeof(binary_tree_t));
+
 	if (!root)
-		return NULL;
+		return (NULL);
 
 	root->parent = parent;
 	root->left = NULL;
