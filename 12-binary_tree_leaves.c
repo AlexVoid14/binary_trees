99327fdd4757ed63195c9661a58d4a2059117f73
AlexVoid14 <fortuinalexander48@gmail.com>
2023-06-02 10:39:00 -0700

    corrections

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,44 +1,20 @@
 #include "binary_trees.h"
-#include "binary_trees.h"
 
 /**
- * binary_tree_leaves - Function that counts the leaves in a binary tree
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the size of.
  *
- * @tree:- pointer to the root node of the tree
- *
- * Return: Count of leaves
+ * Return: The size of the tree.
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	size_t size = 0;
 
-	if (tree->left == NULL && tree->right == NULL)
-		return 1;
-
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-
-	return (left_leaves + right_leaves);
-}
-
-/**
- * binary_tree_leaves - Function that counts the leaves in a binary tree
- *
- * @tree:- pointer to the root node of the tree
- *
- * Return: Count of leaves
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return 1;
-
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-
-	return (left_leaves + right_leaves);
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
 }
