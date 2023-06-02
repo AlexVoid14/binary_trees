cb49a86e3690d7e3e25b2d7de2551741bb86f542
NyatiZw <appsmaid19@gmail.com>
2023-06-01 17:47:27 -0700

    binary tree corrections

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,39 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	const binary_tree_t *current_level[LIMIT];
 
-	if (left_height >= right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	current_level[0] = tree;
+	size_t level_size = 1;
+
+	while (level_size > 0)
+	{
+		const binary_tree_t *next_level[LIMIT];
+		size_t next_level_index = 0;
+
+		for (size_t i = 0; i < level_size; i++)
+		{
+			const binary_tree_t *node = current_level[i];
+
+			if (node->left != NULL)
+				next_level[next_level_index++] = node->left;
+			if (node->right != NULL)
+				next_level[next_level_index++] = node->right;
+		}
+
+		if (next_level_index == 0)
+			break;
+
+		height++;
+		level_size = next_level_index;
+		for (size_t i = 0; i < level_size; i++)
+			current_level[i] = next_level[i];
+	}
+
+	return (height);
 }
