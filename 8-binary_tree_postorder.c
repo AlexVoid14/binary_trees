c391166014a08a6ba2087f836e5989a2546f7aee
NyatiZw <appsmaid19@gmail.com>
2023-06-01 17:17:46 -0700

    binary tree corrections

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - 'Function that goes through tree using post-order traversal'
+ * binary_tree_postorder - 'Function that uses post-order traversal'
  *
  * @tree:- pointer to the root node of the tree
  * @func:- pointer to function calling each node
